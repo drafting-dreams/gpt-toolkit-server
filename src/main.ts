0532fdc8c95566e4ae408dc89d6e222e864ed8ae
shawn_white <draftingdreams666@gmail.com>
2023-06-02 23:43:45 +0800

    feat: settle port

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,19 @@ import { AppModule } from './app.module';
 
 // How to generate these files HTTPS certificate
 // https://stackoverflow.com/questions/22584268/node-js-https-pem-error-routinespem-read-biono-start-line/24283204#24283204
-const httpsOptions = {
-  key: fs.readFileSync('./secrets/key.pem'),
-  cert: fs.readFileSync('./secrets/server.crt'),
-};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions });
-  await app.listen(process.env.PORT);
+  const app = await NestFactory.create(
+    AppModule,
+    process.env.ENV === 'PROD'
+      ? undefined
+      : {
+          httpsOptions: {
+            key: fs.readFileSync('./secrets/key.pem'),
+            cert: fs.readFileSync('./secrets/server.crt'),
+          },
+        },
+  );
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
